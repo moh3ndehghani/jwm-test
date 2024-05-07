4afda2218b3df365aa2c477590880696d663840a
Mohsen Dehghani <m.dehghani@ernyka.com@i3071-dwl.ernyka.com>
2024-05-07 14:50:31 +0330

    add tailwind

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,19 @@
+<template>
+
+  <header class="header">
+    <div class="header-box">
+      <span>Username</span>
+    </div>
+  </header>
+
+</template>
+
+<style scoped lang="scss">
+.header {
+  @apply flex items-center justify-center p-5;
+
+  &-box {
+    @apply w-1/2 bg-slate-100 px-20 py-10 rounded-xl text-center;
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="w-1/2 mx-auto  bg-red-500">
+    home
+  </div>
+</template>
